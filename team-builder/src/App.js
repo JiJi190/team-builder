2e7d41e3483feeffca6d0ebc49f5cb7b1c500424
unknown <robertrey1234@gmail.com>
2019-07-15 16:14:54 -0500

    Structured form to take data

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import './App.css';
+import Form from "./components/Form";
 
 function App() {
   const [teamInfo, setTeamInfo] = useState([])
   return (
     <>
-
+      <Form teamInfo={teamInfo}/>
     </>
   );
 }
--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Form = props => {
+    const [memberData, setMemberData] = useState({
+        name: "",
+        email: "",
+        role: ""
+    })
+
+    const changeHandle = event => {
+        setMemberData({...memberData, [event.target.name]: event.target.value})
+    }
+
+    const submitHandle = event => {
+        event.preventDefault();
+
+    }
+
     return(
         <>
+        <form onSubmit={submitHandle}>
+            <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={changeHandle}
+            value=""
+            />
 
+            
+            <input
+            type="text"
+            name="email"
+            placeholder="E-mail"
+            onChange={changeHandle}
+            value=""
+            />
+
+            <input
+            type="text"
+            name="role"
+            placeholder="Team Role"
+            onChange={changeHandle}
+            value=""
+            />
+        </form>
         </>
     )
 }
